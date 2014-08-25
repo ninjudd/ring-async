f7f0495db2737f90db394ccb97bfda2b05d0254e
Justin Balthrop <git@justinbalthrop.com>
2014-08-25 13:51:41 -0700

    support flushing buffer to client by putting :flush on channel

--- src/ring/util/async.clj
+++ src/ring/util/async.clj
@@ -15,10 +15,12 @@
       (let [^ServletOutputStream out (.getOutputStream servlet-response)]
         (go (loop []
               (when-let [data (<! chan)]
-                (try (.write out data)
-                     (.flush out)
-                     (catch java.io.IOException e
-                       (close! chan)))
+                (if (= :flush data)
+                  (.flushBuffer servlet-response)
+                  (try (.write out data)
+                       (.flush out)                  
+                       (catch java.io.IOException e
+                         (close! chan))))
                 (recur)))
             (.complete async)))
       (dissoc response :body))
