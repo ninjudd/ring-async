4843cd3f2dca35d23d444c7ddcf1be3f6f8efda2
Justin Balthrop <git@justinbalthrop.com>
2014-10-18 09:07:29 -0700

    new release to fix stray exception printing

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ninjudd/ring-async "0.3.1"
+(defproject com.ninjudd/ring-async "0.3.2"
   :description "Ring middleware adding support for asynchronous responses."
   :url "https://github.com/ninjudd/ring-async"
   :license {:name "The MIT License"
